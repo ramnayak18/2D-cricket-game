905ef6411d93bcf8cc00a868a3c10f01e7d4aa80
Namita Kumari <cs20btech11034@iith.ac.in>
2021-07-23 11:51:31 +0530

    Fixed #4

--- menuWindow.cpp
+++ menuWindow.cpp
@@ -35,10 +35,12 @@ void menuWindow::reset()
 {
     _tile[0]->state = "Hover";
     _tile[0]->tiles.setFillColor(sf::Color::Red);
+    _tile[0]->updateColor();
     for(int i=1;i<4;i++)
     {
         _tile[i]->state = "NULL";
         _tile[i]->updateColor();
+        _tile[i]->tiles.setFillColor(sf::Color::Yellow);
     }
     event.key.code = sf::Keyboard::Space;
 }
